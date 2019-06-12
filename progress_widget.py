4eca5f20b967e92c8a723c16c9e244d3c7a14066
voightp <vojtechpanek@seznam.cz>
2019-06-12 22:27:10 +0100

    working on progress

--- progress_widget.py
+++ progress_widget.py
@@ -17,13 +17,13 @@ class MyStatusBar(QStatusBar):
     def set_max_value(self, monitor_id, max_value):
         self.widgets[monitor_id].set_maximum(max_value)
 
-    def update_summary(self):
-        n_pending = len(self.pending_jobs)
+    def update_summary(self, n_pending):
         self.active_jobs[-1].update_label(n_pending)
 
     def add_summary_widget(self):
+        n = len(self.pending_jobs)
         wgt = SummaryWidget(self)
-        wgt.update_label(2)
+        wgt.update_label(n)
         self.active_jobs.append(wgt)
         self.addWidget(wgt)
 
@@ -32,46 +32,57 @@ class MyStatusBar(QStatusBar):
         self.removeWidget(wgt)
         self.active_jobs.remove(wgt)
 
+    def move_widget(self, wgt):
+        self.removeWidget(wgt)
+        self.active_jobs.remove(wgt)
+        self.pending_jobs.insert(0, wgt)
+
+    def add_widget(self, wgt):
+        self.addWidget(wgt)
+        self.active_jobs.append(wgt)
+
     def add_file(self, id_, name):
         wgt = ProgressWidget(self, name)
         wgt.set_pending()
         self.widgets[id_] = wgt
 
         n_active = len(self.active_jobs)
+        n_pending = len(self.pending_jobs)
 
         if n_active <= self.max_active_jobs:
-            self.active_jobs.append(wgt)
-            self.addWidget(wgt)
+            self.add_widget(wgt)
 
         elif n_active == (self.max_active_jobs + 1):
-            last_wgt = self.active_jobs.pop(-1)
-            self.pending_jobs.insert(0, last_wgt)
-            self.pending_jobs.insert(1, wgt)
+            self.pending_jobs.insert(0, wgt)
+
+            last_wgt = self.active_jobs[-1]
+            self.move_widget(last_wgt)
             self.add_summary_widget()
+
         else:
             self.pending_jobs.insert(0, wgt)
-            self.update_summary()
+            self.update_summary(n_pending)
+
+        print("FILE ADDED N PENDING {} N ACTIVE {}".format(len(self.pending_jobs), len(self.active_jobs)))
 
     def update_progress(self, id_, val):
         self.widgets[id_].progress_bar.setValue(val)
 
     def file_loaded(self, id_):
+        print("FILE LOADED N PENDING {} N ACTIVE {}".format(len(self.pending_jobs), len(self.active_jobs)))
         del_wgt = self.widgets[id_]
         del_wgt.deleteLater()
+        self.active_jobs.remove(del_wgt)
         del self.widgets[id_]
 
         n_pending = len(self.pending_jobs)
-
-        if n_pending == 1:
-            self.remove_summary_widget()
-
-
-        try:
-            wgt = .pop(0)
-        except IndexError:
-            return
-
         n_active = len(self.active_jobs)
+
+        if n_pending == 1 and n_active == 4:
+            self.remove_summary_widget()
+            new_wgt = self.pending_jobs.pop(0)
+            self.active_jobs.insert(0, new_wgt)
+            self.addWidget(new_wgt)
 
 
 class SummaryWidget(QWidget):
