e7f9a950c133e6c2653039f03b4ef468943ecbc9
voightp <vojtechpanek@seznam.cz>
2019-06-15 20:02:48 +0100

    revamped widget display (needs finishing)

--- progress_widget.py
+++ progress_widget.py
@@ -4,7 +4,7 @@ from queue import Queue
 
 
 class MyStatusBar(QStatusBar):
-    max_active_jobs = 4
+    max_active_jobs = 5
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -14,8 +14,43 @@ class MyStatusBar(QStatusBar):
         self._active_jobs = []
         self._pending_jobs = []
 
+        self._visible = []
+        self.summary_wgt = SummaryWidget(self)
+        self.summary_wgt.set_pending()
+
+    @property
+    def sorted_wgts(self):
+        wgts = list(self.widgets.values())
+        return sorted(wgts, key=lambda x: x.value())
+
     def set_max_value(self, monitor_id, max_value):
         self.widgets[monitor_id].set_maximum(max_value)
+
+    def render_stuff(self):
+        wgts = self.sorted_wgts
+        max_ = self.max_active_jobs
+        vis = self._visible
+
+        if wgts != vis:
+            for w in vis:
+                self.removeWidget(w)
+
+            disp = wgts[0:max_]
+            if len(wgts) > max_:
+                disp = wgts[0:(max_ - 1)]
+                n = len(wgts) - len(disp)
+                self.summary_wgt.update_label(n)
+                disp.append(self.summary_wgt)
+
+            m = []
+            vis.clear()
+            for d in disp:
+                m.append((d.label.text(), d.value()))
+                vis.append(d)
+                d.show()
+                self.addWidget(d)
+
+            print(m)
 
     def update_progress(self, id_, val):
         self.widgets[id_].progress_bar.setValue(val)
@@ -36,58 +71,76 @@ class MyStatusBar(QStatusBar):
         wgt.update_label(n_pending)
         return wgt
 
-    def handle_adding_summary(self, wgt, active, pending):
-        self.removeWidget(wgt)
-        pending.insert(0, wgt)
-
-        sum_wgt = self.create_summary_widget(len(pending))
-        active.append(sum_wgt)
-        self.add_widget(sum_wgt)
-
     def add_file(self, id_, name):
         wgt = ProgressWidget(self)
         wgt.set_label(name)
         wgt.set_pending()
         self.widgets[id_] = wgt
 
-        a = self._active_jobs
-        p = self._pending_jobs
-        max_ = self.max_active_jobs
+        self.render_stuff()
 
-        if len(a) < max_:
-            self.add_widget(wgt)
+        # print("ADDING", len(a), len(p)) # TODO remove this
 
-        elif len(a) == max_ and len(p) == 0:
-            p.insert(0, wgt)
-            last_wgt = a.pop(-1)
-            self.handle_adding_summary(last_wgt, a, p)
+        # if len(a) < max_:
+        #     self.add_widget(wgt)
+        #
+        # elif len(a) == max_ and len(p) == 0:
+        #     p.append(wgt)
+        #     last_wgt = a.pop(-1)
+        #
+        #     self.removeWidget(last_wgt)
+        #     p.insert(0, last_wgt)
+        #
+        #     sum_wgt = self.create_summary_widget(len(p))
+        #     a.append(sum_wgt)
+        #     self.addWidget(sum_wgt)
+        #
+        # else:
+        #     p.append(wgt)
+        #     self.update_summary(len(p))
 
-        else:
-            p.insert(0, wgt)
-            self.update_summary(len(p))
+        # print("ADDING DONE", len(a), len(p))# TODO remove this
 
     def file_loaded(self, id_):
         del_wgt = self.widgets[id_]
         del_wgt.deleteLater()
+        del self.widgets[id_]
 
         p = self._pending_jobs
         a = self._active_jobs
 
-        if del_wgt in a:
-            a.remove(del_wgt)
-            if len(p) > 1:
-                wgt = p.pop(0)
-                self.add_widget(wgt)
-                self.update_summary(len(p))
+        try:
+            self._visible.remove(del_wgt)
+        except ValueError:
+            print("FOO")
 
-        else:
-            p.remove(del_wgt)
-            if len(p) == 1:
-                self.remove_summary_widget(a)
-            else:
-                self.update_summary(len(p))
+        self.render_stuff()
 
-        del self.widgets[id_]
+        # print("FILE LOADED", len(a), len(p)) # TODO remove this
+
+        # if del_wgt in a:
+        #     a.remove(del_wgt)
+        #     if len(p) > 0:
+        #         wgt = p.pop(0)
+        #         if len(p) == 1:
+        #             self.remove_summary_widget(a)
+        #             wg = p.pop(0)
+        #             self.add_widget(wg)
+        #         else:
+        #             self.update_summary(len(p))
+        #
+        #         self.add_widget(wgt)
+        #
+        # else:
+        #     p.remove(del_wgt)
+        #     if len(p) == 1:
+        #         self.remove_summary_widget(a)
+        #         wg = p.pop(0)
+        #         self.add_widget(wg)
+        #     else:
+        #         self.update_summary(len(p))
+
+        # print("FILE LOADED DONE", len(a), len(p))# TODO remove this
 
 
 class ProgressWidget(QWidget):
@@ -98,7 +151,7 @@ class ProgressWidget(QWidget):
         layout.setSpacing(0)
 
         self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
-        self.setFixedWidth(200)
+        self.setFixedWidth(140)
 
         self.progress_bar = QProgressBar()
         self.progress_bar.setTextVisible(False)
@@ -120,6 +173,9 @@ class ProgressWidget(QWidget):
 
     def set_label(self, text):
         self.label.setText(text)
+
+    def value(self):
+        return self.progress_bar.value()
 
     def set_failed_state(self):
         pass
