0148ea47330257f1d05f4257ea528e1e8ad83ce5
voightp1 <vojtechpanek@seznam.cz>
2019-07-03 16:07:59 +0100

    painting some icons

--- icon_painter.py
+++ icon_painter.py
@@ -1,10 +1,14 @@
-from PySide2.QtWidgets import QApplication
-from PySide2.QtCore import Qt
-from PySide2.QtGui import QPixmap
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtCore import Qt, QPoint
+from PySide2.QtGui import QPixmap, QPainter, QFont, QImage, QFontDatabase
 import sys
-from icons import Pixmap
+from icons import Pixmap, text_to_pixmap
+from app import install_fonts
 
 app = QApplication()
+db = QFontDatabase()
+install_fonts("./resources", db)
+db.addApplicationFont("./resources/Roboto-Regular.ttf")
 
 col = dict(r=255, g=255, b=255)
 # f1 = Pixmap("./icons/chevron_right.png", **col)
@@ -13,7 +17,5 @@ col = dict(r=255, g=255, b=255)
 # f1.save("./icons/chevron_right_white.png")
 # f2.save("./icons/chevron_left_white.png")
 
-tr = QPixmap(16, 16)
-tr.fill(Qt.transparent)
-tr.save("./icons/transparent.png")
+
 
