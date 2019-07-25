f73da3b201fd5fe8333641f8a72b322e00979234
voightp <vojtechpanek@seznam.cz>
2019-07-25 21:22:28 +0100

    update appearance function moved to 'widgets'

--- widgets.py
+++ widgets.py
@@ -1,6 +1,53 @@
 from PySide2.QtWidgets import QTextEdit, QSizePolicy, QLineEdit, QFrame, QHBoxLayout
 from PySide2.QtGui import QTextOption
-from PySide2.QtCore import Qt, Signal
+from PySide2.QtCore import Qt, Signal, QFileInfo
+from PySide2.QtWidgets import QFrame
+from PySide2 import QtGui, QtCore
+
+
+def update_appearance(wgt):
+    """ Refresh CSS of the widget. """
+    wgt.style().unpolish(wgt)
+    wgt.style().polish(wgt)
+
+
+class DropFrame(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAcceptDrops(True)
+        self.setMouseTracking(True)
+
+    def dragEnterEvent(self, event):
+        print("Drag enter event.")
+        mime = event.mimeData()
+
+        if not mime.hasUrls():
+            return
+
+        files = []
+        for url in mime.urls():
+            f = url.toLocalFile()
+            info = QFileInfo(f)
+            if info.suffix() in ("eso",):
+                files.append(f)
+
+        if files:
+            print(files)
+
+        event.acceptProposedAction()
+
+    def dragMoveEvent(self, event):
+        pass
+
+    def dragLeaveEvent(self, event):
+        print("Drag leave event.")
+
+    def dropEvent(self, event):
+        print("Drop event.")
+
+    def mouseDoubleClickEvent(self, event):
+        print("double click")
+
 
 # TODO this might not be necessary
 # the reason behind this is that I'd like to find a way to hack cursor color
