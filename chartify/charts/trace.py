d8c9a1d9fd1e7618f4843542e16b3389ffb5863d
voightp <vojtechpanek@seznam.cz>
2019-11-24 17:59:01 +0000

    added TraceData

--- chartify/charts/trace.py
+++ chartify/charts/trace.py
@@ -1,3 +1,20 @@
+class TraceData:
+    def __init__(self, item_id, name, primary_data, primary_data_type,
+                 secondary_data=None, secondary_data_type=None):
+        self.item_id = item_id
+        self.name = name
+        self.primary_data = primary_data
+        self.primary_data_type = primary_data_type
+        self.secondary_data = secondary_data
+        self.secondary_data_type = secondary_data_type
+
+
+class Trace2D:
+    def __init__(self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
+
+
 class Trace:
     def __init__(self, item_id, trace_id, file_name,
                  interval, key, variable, units, values, total_value,
