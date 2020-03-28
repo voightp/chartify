cfc0dea69472a7b1ba1504933ad01efcbc1980be
voightp <vojtechpanek@seznam.cz>
2020-03-28 11:21:11 +0000

    updated typehints

--- chartify/ui/icons.py
+++ chartify/ui/icons.py
@@ -1,5 +1,7 @@
-from PySide2.QtCore import Qt, QBuffer, QTemporaryFile, QRectF
-from PySide2.QtGui import QImage, QPixmap, QColor, QPainter, QFontMetrics, QPen
+from typing import Union, Tuple
+
+from PySide2.QtCore import Qt, QBuffer, QTemporaryFile, QRectF, QSize
+from PySide2.QtGui import QImage, QPixmap, QColor, QPainter, QFontMetrics, QPen, QFont
 
 
 class Pixmap(QPixmap):
@@ -12,14 +14,14 @@ class Pixmap(QPixmap):
     overridden using specified RGB values.
     """
 
-    def __init__(self, path, r=0, g=0, b=0, a=1):
+    def __init__(self, path, r: int = 0, g: int = 0, b: int = 0, a: float = 1):
         path = path if isinstance(path, str) else str(path)
         super().__init__(path)
 
         if not (r == 0 and g == 0 and b == 0 and a == 1):
             self.repaint(path, r, g, b, a)
 
-    def repaint(self, path, r, g, b, a):
+    def repaint(self, path: str, r: int, g: int, b: int, a: float) -> None:
         """ Repaint all non-transparent pixels with given color. """
         img = QImage(path)
         for x in range(img.width()):
@@ -31,7 +33,7 @@ class Pixmap(QPixmap):
 
         self.convertFromImage(img)
 
-    def as_temp(self):
+    def as_temp(self) -> QTemporaryFile:
         """ Save the pixmap as a temporary file. """
         buff = QBuffer()
         self.save(buff, "PNG")
@@ -43,7 +45,7 @@ class Pixmap(QPixmap):
         return tf
 
 
-def text_to_pixmap(text, font, color, size=None):
+def text_to_pixmap(text: str, font: QFont, color: QColor, size=None):
     """ Convert text to QPixmap of a given size. """
 
     def text_geometry():
@@ -65,18 +67,25 @@ def text_to_pixmap(text, font, color, size=None):
     return pix
 
 
-def filled_circle_pixmap(size, c1, c2=None, border_col=None, border_w=1, fr=0.7):
-    """ Draw a pixmap with single or two colors filled circle. """
+def filled_circle_pixmap(
+        size: QSize,
+        c1: QColor,
+        c2: QColor = None,
+        border_color: QColor = None,
+        border_width: int = 1,
+        fraction: float = 0.7
+) -> QPixmap:
+    """ Draw a pixmap with one or two colors filled circle. """
     pix = QPixmap(size)
     pix.fill(Qt.transparent)
 
     p = QPainter(pix)
 
     w, h = size.width(), size.height()
-    x = (w - (fr * w)) / 2
-    y = (h - (fr * h)) / 2
-    rect_w = w * fr
-    rect_y = h * fr
+    x = (w - (fraction * w)) / 2
+    y = (h - (fraction * h)) / 2
+    rect_w = w * fraction
+    rect_y = h * fraction
 
     rect = QRectF(x, y, rect_w, rect_y)
     p.setBrush(c1)
@@ -90,10 +99,10 @@ def filled_circle_pixmap(size, c1, c2=None, border_col=None, border_w=1, fr=0.7)
         p.setBrush(c2)
         p.drawChord(rect, -90 * 16, 180 * 16)
 
-    if border_col:
+    if border_color:
         # draw the border
         p.setBrush(Qt.NoBrush)
-        p.setPen(QPen(border_col, border_w))
+        p.setPen(QPen(border_color, border_width))
         p.drawChord(rect, 0, 360 * 16)
 
     p.end()
@@ -101,16 +110,14 @@ def filled_circle_pixmap(size, c1, c2=None, border_col=None, border_w=1, fr=0.7)
     return pix
 
 
-def combine_colors(c1: tuple, c2: tuple, fr: float, as_tuple=False):
+def combine_colors(
+        c1: tuple, c2: tuple, fraction: float, as_tuple=False
+) -> Union[str, Tuple[int, int, int]]:
     """ Combine given colors. """
     # colors need to be passed as rgb tuple
     # fr define fraction of the first color
     rgb = []
     for i in range(3):
-        c = c1[i] * fr + c2[i] * (1 - fr)
+        c = c1[i] * fraction + c2[i] * (1 - fraction)
         rgb.append(int(c))
-
-    if as_tuple:
-        return tuple(rgb)
-    else:
-        return f"rgb({', '.join([str(c) for c in rgb])})"
+    return tuple(rgb) if as_tuple else f"rgb({', '.join([str(c) for c in rgb])})"
