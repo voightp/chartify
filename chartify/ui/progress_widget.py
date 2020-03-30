110eed6cb30b06b558559e39e799e4860f008332
voightp <vojtechpanek@seznam.cz>
2020-03-30 21:35:25 +0100

    added type hints, added tests

--- chartify/ui/progress_widget.py
+++ chartify/ui/progress_widget.py
@@ -1,4 +1,5 @@
 import contextlib
+from typing import List, Optional
 
 from PySide2.QtCore import Signal, Qt
 from PySide2.QtWidgets import (
@@ -15,182 +16,21 @@ from PySide2.QtWidgets import (
 from chartify.utils.utils import refresh_css
 
 
-class ProgressContainer(QWidget):
-    """
-
-    A container to hold all progress widgets.
-
-    All the currently processed files are stored as 'masks'.
-    Masks data is being updated using signals mechanism from
-    application controller.
-
-    """
-
-    MAX_VISIBLE_JOBS = 5
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        layout = QHBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setAlignment(Qt.AlignLeft)
-
-        widgets = []
-        for i in range(self.MAX_VISIBLE_JOBS):
-            wgt = ProgressWidget(self)
-            wgt.remove.connect(self.remove_file)
-            wgt.setVisible(False)
-            widgets.append(wgt)
-            self.layout().addWidget(wgt)
-
-        self.widgets = widgets
-        self.files = {}
-        self.locked = []
-
-    @property
-    def sorted_files(self):
-        """ Sort widgets by value (descending order). """
-        all_files = list(self.files.values())
-
-        # remove already locked files
-        files = list(filter(lambda x: x not in self.locked, all_files))
-
-        # locked files take precedent
-        sorted_files = self.locked + sorted(files, key=lambda x: x.rel_value, reverse=True)
-
-        return sorted_files
-
-    @property
-    def visible_files(self):
-        """ Get currently visible files. """
-        wgts = self.visible_widgets
-        return [wgt.file_ref for wgt in wgts]
-
-    @property
-    def visible_widgets(self):
-        """ Get currently visible widgets. """
-        return list(filter(lambda x: x.file_ref, self.widgets))
-
-    def _get_visible_index(self, file):
-        """ Get visible index, returns 'None' if invalid. """
-        try:
-            return self.visible_files.index(file)
-        except ValueError:
-            return None
-
-    def _position_changed(self, file):
-        """ Check if the current widget triggers repositioning. """
-        pos = self.sorted_files.index(file)
-        i = self._get_visible_index(file)
-
-        if i is None:
-            # widget is in pending section, although it
-            # can still be being processed on machines with
-            # number of cpu greater than MAX_VISIBLE_JOBS
-            vals = [v.rel_value for v in self.visible_files if not isinstance(v, SummaryFile)]
-            return any(map(lambda x: x < (file.rel_value + 3), vals))
-
-        return pos != i
-
-    def _update_bar(self):
-        """ Update progress widget order on the status bar. """
-        files = self.sorted_files
-        widgets = self.widgets
-        max_ = self.MAX_VISIBLE_JOBS
-        n = len(files)
-
-        fill = [None for _ in range(n, max_)]
-        disp = files[0:max_] if n > max_ else files + fill
-        if n > max_:
-            n = n - max_ + 1
-            sm = SummaryFile()
-            sm.update_label(n)
-            disp[-1] = sm
-
-        for f, w in zip(disp, widgets):
-            if not f:
-                w.file_ref = None
-            elif f != w.file_ref:
-                w.set_file_ref(f)
-
-        for w in widgets:
-            w.setVisible(bool(w.file_ref))
-
-    def add_file(self, id_, name):
-        """ Add progress file to the container. """
-        self.files[id_] = ProgressFile(id_, name)
-        self._update_bar()
-
-    def set_range(self, id_, value, max_value):
-        """ Set up maximum progress value. """
-        with contextlib.suppress(KeyError):
-            f = self.files[id_]
-            f.maximum = max_value
-            f.value = value
-
-            i = self._get_visible_index(f)
-            if i is not None:
-                self.widgets[i].update_max()
-
-    def update_progress(self, id_, value):
-        """ Update file progress. """
-        with contextlib.suppress(KeyError):
-            f = self.files[id_]
-            f.value = value
-
-            i = self._get_visible_index(f)
-            if i is not None:
-                self.widgets[i].update_value()
-
-            if self._position_changed(f):
-                self._update_bar()
-
-    def set_failed(self, id_, message):
-        """ Set failed status on the given file. """
-        file = self.files[id_]
-        file.set_failed()
-        i = self._get_visible_index(file)
-
-        if file not in self.locked:
-            # let failed files be always visible
-            self.locked.append(file)
-
-        if i is not None:
-            self.widgets[i].update_all_values()
-            self.widgets[i].set_failed_status(message)
-
-    def set_pending(self, id_):
-        """ Set pending status on the given file. """
-        file = self.files[id_]
-        file.set_pending()
-
-        if file not in self.locked:
-            # pending files become locked so their position does not change
-            # condition is in place to avoid multiple references when calling
-            # set_pending multiple times
-            self.locked.append(file)
-
-        i = self._get_visible_index(self.files[id_])
-        if i is not None:
-            self.widgets[i].update_all_values()
-
-    def remove_file(self, id_):
-        """ Remove file from the container. """
-        del_file = self.files.pop(id_)
-
-        with contextlib.suppress(ValueError):
-            self.locked.remove(del_file)
-
-        i = self._get_visible_index(del_file)
-        if i is not None:
-            self.widgets[i].file_ref = None
-
-        self._update_bar()
-
-
 class ProgressFile:
-    """
-    Helper to store file progress details.
+    """ Helper to store file progress details.
+
+    Attributes
+    ----------
+    id_ : str
+        File identifier.
+    label : str
+        Text displayed on the widget.
+    maximum : int
+        Maximum progress set on progress bar.
+    value: int
+        Current progress value.
+    failed : bool, default False
+        Checks if processing failed.
 
     """
 
@@ -223,11 +63,23 @@ class ProgressFile:
 
 
 class SummaryFile:
-    """
-    A special type of progress file to report
-    remaining number of jobs.
+    """ A special type of progress file to report remaining number of jobs.
 
     The status is always pending.
+
+    Attributes
+    ----------
+    maximum : int, 0
+        Maximum is always zero in order to force 'pending' status.
+    value : int, 0
+        Value is always zero in order to force 'pending' status.
+    label : str
+        Text displayed on the widget.
+    file_ref : str, 'summary'
+        Summary file does not hold any file reference.
+    failed : bool, False
+        This is always False as summary file only displays information
+        about remaining number of files.
 
     """
 
@@ -240,13 +92,16 @@ class SummaryFile:
 
     def update_label(self, n) -> None:
         """ Update number of pending jobs. """
-        self.label = "processing {} files...".format(n)
+        self.label = f"processing {n} files..."
 
 
 class ProgressWidget(QFrame):
-    """
-    A widget to display current eso file
-    processing progress.
+    """ A widget to display current eso file processing progress.
+
+    Attributes
+    ----------
+    file_ref : ProgressFile
+        Currently assigned file reference.
 
     """
 
@@ -287,7 +142,7 @@ class ProgressWidget(QFrame):
         self.main_layout.addWidget(self.file_btn)
         self.main_layout.addWidget(wgt)
 
-    def set_file_ref(self, file):
+    def set_file_ref(self, file: ProgressFile) -> None:
         """ Update widget properties. """
         self.file_ref = file
         self.update_all_values()
@@ -298,36 +153,208 @@ class ProgressWidget(QFrame):
             # widget has been in 'failed' state, reapply standard appearance
             self.set_normal_status()
 
-    def update_all_values(self):
+    def update_all_values(self) -> None:
         """ Refresh all attributes. """
         self.update_label()
         self.update_max()
         self.update_value()
 
-    def update_max(self):
+    def update_max(self) -> None:
         """ Set progress bar maximum value. """
         self.progress_bar.setMaximum(self.file_ref.maximum)
 
-    def update_label(self):
+    def update_label(self) -> None:
         """ Set text on the label. """
         self.label.setText(self.file_ref.label)
 
-    def update_value(self):
+    def update_value(self) -> None:
         """ Set current value. """
         self.progress_bar.setValue(self.file_ref.value)
 
-    def set_normal_status(self):
+    def set_normal_status(self) -> None:
         """ Apply standard style. """
         self.setProperty("failed", "false")
         self.setToolTip("")
         refresh_css(self.label, self.progress_bar, self.file_btn)
 
-    def set_failed_status(self, message):
+    def set_failed_status(self, message) -> None:
         """ Apply 'failed' style. """
         self.setProperty("failed", "true")
         self.setToolTip(message)
         refresh_css(self.label, self.progress_bar, self.file_btn)
 
-    def send_remove_me(self):
+    def send_remove_me(self) -> None:
         """ Give signal to status bar to remove this widget. """
         self.remove.emit(self.file_ref.id_)
+
+
+class ProgressContainer(QWidget):
+    """ A container to hold all progress widgets.
+
+    All the currently processed files are stored as 'masks'.
+    Masks data is being updated using signals mechanism from
+    application controller.
+
+    Attributes
+    ----------
+    widgets : List of ProgressWidget
+        Widgets to visually represent file information.
+    files : Dict of int, ProgressFile
+        Holds file reference for all processed files.
+    locked List of ProgressFile
+        Stores files with locked position.
+
+    """
+
+    MAX_VISIBLE_JOBS = 5
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setAlignment(Qt.AlignLeft)
+
+        widgets = []
+        for i in range(self.MAX_VISIBLE_JOBS):
+            wgt = ProgressWidget(self)
+            wgt.remove.connect(self.remove_file)
+            wgt.setVisible(False)
+            widgets.append(wgt)
+            self.layout().addWidget(wgt)
+
+        self.widgets = widgets
+        self.files = {}
+        self.locked = []
+
+    @property
+    def sorted_files(self) -> List[ProgressFile]:
+        """ Sort widgets by value (descending order). """
+        all_files = list(self.files.values())
+
+        # remove already locked files
+        files = list(filter(lambda x: x not in self.locked, all_files))
+
+        # locked files take precedent
+        sorted_files = self.locked + sorted(files, key=lambda x: x.rel_value, reverse=True)
+
+        return sorted_files
+
+    @property
+    def visible_files(self) -> List[ProgressFile]:
+        """ Get currently visible files. """
+        # list widgets holding a file reference
+        return [wgt.file_ref for wgt in filter(lambda x: x.file_ref, self.widgets)]
+
+    def _get_visible_index(self, file: ProgressFile) -> Optional[int]:
+        """ Get visible index, returns 'None' if invalid. """
+        with contextlib.suppress(ValueError):
+            return self.visible_files.index(file)
+
+    def _position_changed(self, file: ProgressFile) -> bool:
+        """ Check if the current widget triggers repositioning. """
+        pos = self.sorted_files.index(file)
+        i = self._get_visible_index(file)
+
+        if i is None:
+            # widget is in pending section, although it
+            # can still be being processed on machines with
+            # number of cpu greater than MAX_VISIBLE_JOBS
+            vals = [v.rel_value for v in self.visible_files if not isinstance(v, SummaryFile)]
+            return any(map(lambda x: x < (file.rel_value + 3), vals))
+
+        return pos != i
+
+    def _update_bar(self) -> None:
+        """ Update progress widget order on the status bar. """
+        max_ = self.MAX_VISIBLE_JOBS
+        n = len(self.sorted_files)
+
+        fill = [None for _ in range(n, max_)]
+        displayed = self.sorted_files[0:max_] if n > max_ else self.sorted_files + fill
+        if n > max_:
+            # there's more files than maximum, show summary info
+            n = n - max_ + 1
+            summary_file = SummaryFile()
+            summary_file.update_label(n)
+            displayed[-1] = summary_file
+
+        for f, w in zip(displayed, self.widgets):
+            if not f:
+                w.file_ref = None
+            elif f != w.file_ref:
+                w.set_file_ref(f)
+
+        for w in self.widgets:
+            w.setVisible(bool(w.file_ref))
+
+    def add_file(self, id_: str, name: str) -> None:
+        """ Add progress file to the container. """
+        self.files[id_] = ProgressFile(id_, name)
+        self._update_bar()
+
+    def set_range(self, id_: str, value: int, max_value: int) -> None:
+        """ Set up maximum progress value. """
+        with contextlib.suppress(KeyError):
+            f = self.files[id_]
+            f.maximum = max_value
+            f.value = value
+
+            i = self._get_visible_index(f)
+            if i is not None:
+                self.widgets[i].update_max()
+
+    def update_progress(self, id_: str, value: int) -> None:
+        """ Update file progress. """
+        with contextlib.suppress(KeyError):
+            f = self.files[id_]
+            f.value = value
+
+            i = self._get_visible_index(f)
+            if i is not None:
+                self.widgets[i].update_value()
+
+            if self._position_changed(f):
+                self._update_bar()
+
+    def set_failed(self, id_: str, message: str) -> None:
+        """ Set failed status on the given file. """
+        file = self.files[id_]
+        file.set_failed()
+        i = self._get_visible_index(file)
+
+        if file not in self.locked:
+            # let failed files be always visible
+            self.locked.append(file)
+
+        if i is not None:
+            self.widgets[i].update_all_values()
+            self.widgets[i].set_failed_status(message)
+
+    def set_pending(self, id_: str) -> None:
+        """ Set pending status on the given file. """
+        file = self.files[id_]
+        file.set_pending()
+
+        if file not in self.locked:
+            # pending files become locked so their position does not change
+            # condition is in place to avoid multiple references when calling
+            # set_pending multiple times
+            self.locked.append(file)
+
+        i = self._get_visible_index(self.files[id_])
+        if i is not None:
+            self.widgets[i].update_all_values()
+
+    def remove_file(self, id_: str) -> None:
+        """ Remove file from the container. """
+        del_file = self.files.pop(id_)
+
+        with contextlib.suppress(ValueError):
+            self.locked.remove(del_file)
+
+        i = self._get_visible_index(del_file)
+        if i is not None:
+            self.widgets[i].file_ref = None
+
+        self._update_bar()
--- tests/widgets/test_progress_widget.py
+++ tests/widgets/test_progress_widget.py
@@ -8,52 +8,82 @@ def container(qtbot):
     container = ProgressContainer()
     container.show()
     qtbot.add_widget(container)
+
+    for i in range(1, 9):
+        container.add_file(str(i), f"file-{i}")
+        container.set_range(str(i), 10 * i, 100)
+
     return container
 
 
-def test_sorted_files():
+def test_init(container: ProgressContainer):
+    assert len(container.widgets) == 5
+    assert len(container.files) == 8
+    assert len(container.locked) == 0
+
+
+def test_sorted_files(qtbot, container: ProgressContainer):
+    assert container.sorted_files == list(container.files.values())[::-1]
+
+
+def test_visible_files(qtbot, container: ProgressContainer):
+    summary = container.widgets[-1].file_ref
+    test_files = [
+        container.files[str(7)],
+        container.files[str(6)],
+        container.files[str(5)],
+        container.files[str(4)],
+        summary
+    ]
+    assert container.visible_files == test_files
+
+
+def test__get_visible_index(container: ProgressContainer):
+    file = container.files[str(5)]
+    assert container._get_visible_index(file) == 2
+
+
+def test__get_visible_index_hidden(container: ProgressContainer):
+    file = container.files[str(1)]
+    assert not container._get_visible_index(file)
+
+
+def test__position_changed(container: ProgressContainer):
     assert False
 
 
-def test_visible_files():
+def test__update_bar(container: ProgressContainer):
     assert False
 
 
-def test_visible_widgets():
+def test_add_file(container: ProgressContainer):
     assert False
 
 
-def test__get_visible_index():
+def test_set_range(container: ProgressContainer):
     assert False
 
 
-def test__position_changed():
+def test_update_progress(container: ProgressContainer):
     assert False
 
 
-def test__update_bar():
+def test_set_failed(container: ProgressContainer):
     assert False
 
 
-def test_add_file():
+def test_set_pending(container: ProgressContainer):
     assert False
 
 
-def test_set_range():
+def test_remove_file(container: ProgressContainer):
     assert False
 
 
-def test_update_progress():
-    assert False
-
-
-def test_set_failed():
-    assert False
-
-
-def test_set_pending():
-    assert False
-
-
-def test_remove_file():
-    assert False
+def test_summary_file(container: ProgressContainer):
+    summary = container.widgets[-1].file_ref
+    assert summary.maximum == 0
+    assert summary.value == 0
+    assert summary.label == "processing 4 files..."
+    assert summary.file_ref == "summary"
+    assert summary.failed == False
