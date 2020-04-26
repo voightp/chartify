5b1c3862f2c78f976f87e7f9688db38ccb51945a
voightp <vojtechpanek@seznam.cz>
2020-04-26 16:31:07 +0100

    refactored css processor

--- chartify/ui/css_theme.py
+++ chartify/ui/css_theme.py
@@ -1,6 +1,8 @@
 import json
 import re
+import traceback
 from collections import namedtuple
+from typing import Tuple, Dict, Union, Iterable
 
 from chartify.settings import Settings
 from chartify.ui.icons import Pixmap
@@ -8,9 +10,32 @@ from chartify.ui.icons import Pixmap
 Color = namedtuple("Color", "r, g, b")
 
 
+def parse_color(color: Union[str, Tuple[int, int, int]]) -> Tuple[int, int, int]:
+    """ Get standard plain rgb tuple. """
+    if isinstance(color, tuple) and len(color) == 3:
+        rgb = color
+    elif color.startswith("rgb"):
+        srgb = re.sub("[rgb() ]", "", color)
+        rgb = tuple([int(i) for i in srgb.split(",")])
+    elif color.startswith("#") and len(color) == 7:
+        rgb = tuple([int(color[i: i + 2], 16) for i in range(1, 7, 2)])
+    else:
+        raise TypeError(f"Cannot parse color {color}!")
+    return rgb
+
+
+def parse_palette(pth: str) -> Dict[str, "Palette"]:
+    """ Return a list of palette instances. """
+    palettes = {}
+    with open(pth) as f:
+        ps = json.load(f)
+        for name, p in ps.items():
+            palettes[name] = Palette(name, **p)
+    return palettes
+
+
 class Palette:
-    """
-    A class to define color set used for an application.
+    """ A class to define color set used for an application.
 
     Colors can be defined either using HEX or rgb string
     and rgb tuples.
@@ -28,10 +53,9 @@ class Palette:
         primary_color, primary_text_color, secondary_color, error_color,
         secondary_text_color, background_color, surface_color, ok_color
 
-
     """
 
-    colors = [
+    COLORS = [
         "PRIMARY_COLOR",
         "PRIMARY_TEXT_COLOR",
         "SECONDARY_COLOR",
@@ -42,77 +66,57 @@ class Palette:
         "OK_COLOR",
     ]
 
-    def __init__(self, name, default_color=(255, 255, 255), **kwargs):
+    def __init__(
+            self,
+            name: str,
+            default_color: Tuple[int, int, int] = (255, 255, 255),
+            **kwargs: Union[str, Tuple[int, int, int]]
+    ):
         self.name = name
-        self.colors_dct = self.parse_inst_kwargs(default_color, **kwargs)
-
-    def parse_inst_kwargs(self, default_color, **kwargs):
-        """ Process input kwargs. """
         dct = {}
-
-        for c in self.colors:
+        for c in self.COLORS:
             try:
                 color = kwargs[c.upper()]
                 rgb = parse_color(color)
-                if not rgb:
-                    print(f"'{c}' assigned as default.")
-                    rgb = default_color
-
             except KeyError:
-                print(f"'{c}' has not been provided, " f"assigning default")
+                print(f"'{c}' has not been provided assigning default")
                 rgb = default_color
-
             dct[c] = rgb
+        self.colors_dct = dct
 
-        return dct
-
-    def get_color(self, color_key, opacity=None, as_tuple=False):
+    def get_color(
+            self, color_key: str, opacity: float = None, as_tuple: bool = False
+    ) -> Union[Tuple[int, int, int], Tuple[int, int, int, float], str]:
         """ Get specified color as string. """
         try:
             rgb = self.colors_dct[color_key]
-
             if opacity:
                 # add opacity to rgb request
                 rgb = (*rgb, opacity)
-
             srgb = ",".join([str(i) for i in rgb])
-
             if as_tuple:
                 # this can be rgba
                 return rgb
-
             return f"rgb({srgb})" if len(rgb) == 3 else f"rgba({srgb})"
-
         except KeyError:
-            colors_str = ", ".join(self.colors)
+            colors_str = ", ".join(self.COLORS)
             print(
                 f"Cannot get color for color key '{color_key}' is it's not "
                 f"available, use one of: '{colors_str}'."
             )
 
-    def get_all_colors(self, as_tuple=False):
+    def get_all_colors(
+            self, as_tuple: bool = False
+    ) -> Dict[str, Union[Tuple[int, int, int], str]]:
         """ Get all colors key, color dict (color as string). """
         dct = {}
         for k in self.colors_dct.keys():
             dct[k] = self.get_color(k, as_tuple=as_tuple)
         return dct
 
-    def set_color(self, **kwargs):
-        """ Set specified colors as 'color_key : color' pairs. """
-        for k, v in kwargs.items():
-            try:
-                self.colors_dct[k] = v
-            except KeyError:
-                colors_str = ", ".join(self.colors)
-                print(
-                    f"Cannot set color '{v}', color key '{k}' is not "
-                    f"available, use one of: '{colors_str}'."
-                )
-
 
 class CssTheme:
-    """
-    A class used to parse input css.
+    """ A class used to parse input css.
 
     Lines containing 'palette' color keys or
     images with URL annotation will be parsed.
@@ -133,28 +137,24 @@ class CssTheme:
     """
 
     def __init__(self, *args):
-        self.css_pths = list(args)
+        self.css_paths = list(args)
         self.content = ""
         self._temp = []
 
     @staticmethod
-    def parse_line(line, palette, as_tuple=False):
+    def parse_line(
+            line: str, palette: Palette, as_tuple: bool = False
+    ) -> Union[Tuple[int, int, int], Tuple[int, int, int, float], str]:
         """ Parse a line with color. """
-        key = next(k for k in palette.colors if k in line)
+        key = next(k for k in palette.COLORS if k in line)
         pattern = f"(.*){key}\s?#?(\d\d)?;?"
         prop, opacity = re.findall(pattern, line)[0]
-
         if opacity:
             opacity = round((int(opacity) / 100), 2)
-
         rgb = palette.get_color(key, opacity, as_tuple=as_tuple)
+        return rgb if as_tuple else f"{prop}{rgb};\n"
 
-        if as_tuple:
-            return rgb
-
-        return f"{prop}{rgb};\n"
-
-    def parse_url(self, line, palette):
+    def parse_url(self, line: str, palette: Palette) -> str:
         """ Parse a line with an url. """
         pattern = "(.*)URL\((.*?)\)\s?#(.*);"
         try:
@@ -162,84 +162,33 @@ class CssTheme:
             prop, url, col = tup[0]
             rgb = self.parse_line(col, palette, as_tuple=True)
             if rgb:
+                # a temporary file is required to store repainted pixmap
                 p = Pixmap(str(Settings.ROOT) + url, *rgb)
                 tf = p.as_temp()
                 self._temp.append(tf)
                 line = f"{prop}url({tf.fileName()});\n"
-
-        except IndexError:
-            # this is raised when there's no match
-            print(f"Failed to parse {line}")
-        except ValueError:
-            # this is raised when there's unexpected output
-            print(f"Failed to parse {line}")
-
+        except (IndexError, ValueError):
+            # this is raised when there's no match or unexpected output
+            print(f"Failed to parse {line}.\n{traceback.format_exc()}")
         return line
 
-    def parse_css(self, source_css, palette):
+    def parse_css(self, source_css: Iterable[str], palette: Palette) -> str:
         """ Parse given css files. """
         css = ""
-
         for line in source_css:
             if "URL" in line:
                 line = self.parse_url(line, palette)
-
-            elif any(map(lambda x: x in line, palette.colors)):
+            elif any(map(lambda x: x in line, palette.COLORS)):
                 line = self.parse_line(line, palette)
-
             css += line
-
         return css
 
-    def populate_content(self, palette):
+    def populate_content(self, palette: Palette) -> None:
         """ Update palette. """
-        # temp icons will be created again
-        self._temp.clear()
-
+        self._temp.clear()  # temp icons will be created again
         content = ""
-        for file in self.css_pths:
+        for file in self.css_paths:
             with open(file, "r") as f:
                 css = self.parse_css(f, palette)
                 content += css
-
         self.content = content
-
-
-def parse_color(color: [tuple, str]) -> tuple:
-    """ Get standard plain rgb tuple. """
-    rgb = None
-    if not color:
-        print("Color not specified.")
-        rgb = None
-    elif isinstance(color, tuple) and len(color) == 3:
-        rgb = color
-    elif color.startswith("rgb"):
-        srgb = re.sub("[rgb() ]", "", color)
-        rgb = tuple([int(i) for i in srgb.split(",")])
-    elif color.startswith("#") and len(color) == 7:
-        rgb = tuple([int(color[i : i + 2], 16) for i in range(1, 7, 2)])
-    else:
-        s = color
-        if not isinstance(s, (int, str, float)):
-            #  this is just a basic test
-            s = "".join(s)
-        print(f"Failed to parse color: '{s}'")
-
-    return rgb
-
-
-def parse_palette(pth):
-    """ Return a list of palette instances. """
-    palettes = {}
-
-    try:
-        with open(pth) as f:
-            ps = json.load(f)
-
-            for name, p in ps.items():
-                palettes[name] = Palette(name, **p)
-
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Cannot find palette file '{pth}'.")
-
-    return palettes
