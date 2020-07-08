470a403be77ab7a0b55af58035b41c31103cb918
voightp <vojtechpanek@seznam.cz>
2020-07-08 09:57:00 +0100

    add poetry and update dependencies

--- scripts/build_reader.py
+++ scripts/build_reader.py
@@ -1,12 +1,16 @@
-from pathlib import Path
 import subprocess
+from pathlib import Path
 
 here = Path(__file__).parent
 reader = str(Path(here.parents[1], "esofile-reader"))
 
-process = subprocess.run(f"cd {reader} & python setup.py bdist_wheel", shell=True)
+process = subprocess.run(f"cd {reader} & poetry build", shell=True)
 
 if process.returncode == 0:
-    wheel = [w for w in Path(reader, "dist").iterdir() if w.suffix == ".whl"][0]
-    subprocess.run(f"pip uninstall -y esofile-reader")
+    wheels = [w for w in Path(reader, "dist").iterdir() if w.suffix == ".whl"]
+
+    # pick up only wheel for python 3.8
+    wheel = next(filter(lambda x: "38" in x, wheels))
+
+    subprocess.run("pip uninstall -y esofile-reader")
     subprocess.run(f"pip install {wheel}")
