108fbf155e3e16b43ee6263a34a1def8a08c05b8
voightp <vojtechpanek@seznam.cz>
2021-01-05 18:37:41 +0000

    test VIewMask, add lazy fixture

--- tests/ui/test_view_mask.py
+++ tests/ui/test_view_mask.py
@@ -1,50 +1,162 @@
-from typing import List, Optional, Set, Tuple
-from unittest.mock import patch, MagicMock
+from copy import deepcopy
 
-from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QAction
-from esofile_reader import Variable, SimpleVariable, EsoFile
-
-from chartify.utils.utils import FilterTuple, VariableData
+from chartify.ui.treeview import ViewMask
+from chartify.utils.utils import VariableData
 from tests.conftest import *
+
+VARIABLES = [
+    VariableData("BOILER", "Boiler Ancillary Electric Power", "W"),
+    VariableData("BOILER", "Boiler Gas Rate", "W"),
+]
+
+SIMPLE_VARIABLES = [
+    VariableData("Boiler Gas Rate", None, "W"),
+    VariableData("Boiler Ancillary Electric Power", None, "W"),
+]
+
+
+@pytest.fixture(autouse=True)
+def reset_cached():
+    cached = deepcopy(ViewMask._cached)
+    yield
+    ViewMask._cached = cached
+
+
+@pytest.fixture
+def modified_treeview(qtbot, mw_combined_file):
+    mw_combined_file.on_table_change_requested("hourly")
+    mw_combined_file.current_view.reorder_columns(("key", "type", "proxy_units", "units"))
+    mw_combined_file.current_view.expandAll()
+    mw_combined_file.current_view.select_variables(VARIABLES)
+    mw_combined_file.current_view.update_scrollbar_position(10)
+    mw_combined_file.current_view.header().resizeSection(0, 150)
+    mw_combined_file.on_table_change_requested("daily")
+    return mw_combined_file.current_view
+
+
+@pytest.fixture
+def modified_view(qtbot, mw_combined_file):
+    mw_combined_file.on_table_change_requested("hourly")
+    mw_combined_file.tree_act.setChecked(False)
+    mw_combined_file.current_view.reorder_columns(("key", "type", "proxy_units", "units"))
+    mw_combined_file.current_view.select_variables(VARIABLES)
+    mw_combined_file.current_view.header().resizeSection(1, 180)
+    mw_combined_file.current_view.update_scrollbar_position(8)
+    mw_combined_file.on_table_change_requested("daily")
+    return mw_combined_file.current_view
+
+
+@pytest.fixture
+def modified_simpleview(mw_combined_file):
+    mw_combined_file.current_view.reorder_columns(("proxy_units", "units", "key"))
+    mw_combined_file.current_view.select_variables(SIMPLE_VARIABLES)
+    mw_combined_file.current_view.update_scrollbar_position(5)
+    mw_combined_file.on_table_change_requested("daily-simple")
+    return mw_combined_file.current_view
+
+
+@pytest.fixture
+def initial_treeview(qtbot, mw_combined_file):
+    mw_combined_file.on_table_change_requested("hourly")
+    return mw_combined_file.current_view
+
+
+@pytest.fixture
+def initial_simpleview(qtbot, mw_combined_file):
+    return mw_combined_file.current_view
 
 
 @pytest.mark.parametrize(
-    "similar,old_pos, current_pos,expected_pos,ref_expanded,current_expanded,expected_expanded",
+    "view, header",
     [
-        (True, 123, 321, 123, {"A", "B"}, {"C", "D"}, {"A", "B"}),
-        (False, 123, 321, 321, {"A", "B"}, {"C", "D"}, {"C", "D"}),
+        (pytest.lazy_fixture("modified_treeview"), ("key", "type", "proxy_units", "units"),),
+        (pytest.lazy_fixture("modified_view"), ("key", "type", "proxy_units", "units"),),
+        (pytest.lazy_fixture("modified_simpleview"), ("proxy_units", "units", "key"),),
+        (pytest.lazy_fixture("initial_treeview"), ("type", "key", "proxy_units", "units"),),
+        (pytest.lazy_fixture("initial_simpleview"), ("key", "proxy_units", "units"),),
     ],
 )
-def test_update_view_visual(
-    mw,
-    similar: bool,
-    old_pos: int,
-    current_pos: int,
-    expected_pos: int,
-    ref_expanded: Set[str],
-    current_expanded: Set[str],
-    expected_expanded: Set[str],
-):
-    old_model = MagicMock()
-    old_model.is_similar.return_value = similar
-    old_model.scroll_position = old_pos
-    old_model.expanded = ref_expanded
-    with patch("chartify.ui.main_window.MainWindow.current_view") as current_view:
-        current_view.source_model.scroll_position = current_pos
-        current_view.source_model.expanded = current_expanded
-        current_view.view_type = "tree"
-        var = VariableData("Temperature", "Zone A", "C")
-        mw.update_view_visual(
-            selected=[var], scroll_to=var, old_model=old_model, hide_source_units=False
-        )
-        current_view.update_appearance.assert_called_with(
-            widths={"fixed": 60, "interactive": 200},
-            header=["type", "key", "proxy_units", "units"],
-            filter_tuple=FilterTuple(key="", type="", proxy_units=""),
-            expanded=expected_expanded,
-            selected=[var],
-            scroll_pos=expected_pos,
-            scroll_to=var,
-            hide_source_units=False,
-        )
+def test_header(qtbot, view, header):
+    assert header == view.get_visual_column_data()
+
+
+@pytest.mark.parametrize(
+    "view, widths",
+    [
+        (pytest.lazy_fixture("modified_treeview"), {"fixed": 60, "interactive": 150},),
+        (pytest.lazy_fixture("modified_view"), {"fixed": 60, "interactive": 180},),
+        (pytest.lazy_fixture("modified_simpleview"), {"fixed": 60,},),
+        (pytest.lazy_fixture("initial_treeview"), {"fixed": 60, "interactive": 200},),
+        (pytest.lazy_fixture("initial_simpleview"), {"fixed": 60,},),
+    ],
+)
+def test_widths(qtbot, view, widths):
+    assert widths == view.get_widths()
+
+
+@pytest.mark.parametrize(
+    "view, selected",
+    [
+        (pytest.lazy_fixture("modified_treeview"), VARIABLES,),
+        (pytest.lazy_fixture("modified_view"), VARIABLES,),
+        (pytest.lazy_fixture("modified_simpleview"), SIMPLE_VARIABLES,),
+        (pytest.lazy_fixture("initial_treeview"), [],),
+        (pytest.lazy_fixture("initial_simpleview"), [],),
+    ],
+)
+def test_selected(qtbot, view, selected):
+    assert set(selected) == set(view.get_selected_variable_data())
+
+
+@pytest.mark.parametrize(
+    "view, pos",
+    [
+        (pytest.lazy_fixture("modified_treeview"), 10,),
+        (pytest.lazy_fixture("modified_view"), 8,),
+        (pytest.lazy_fixture("modified_simpleview"), 5,),
+        (pytest.lazy_fixture("initial_treeview"), 0,),
+        (pytest.lazy_fixture("initial_simpleview"), 0,),
+    ],
+)
+def test_scroll_position(qtbot, view, pos):
+    assert pos == view.source_model.scroll_position
+
+
+@pytest.mark.parametrize(
+    "view, expanded",
+    [
+        (
+            pytest.lazy_fixture("modified_treeview"),
+            {
+                "BLOCK1:ZONEA",
+                "BLOCK1:ZONEA FAN COIL UNIT COOLING COIL",
+                "BLOCK1:ZONEB",
+                "BLOCK1:ZONEB FAN COIL UNIT COOLING COIL",
+                "BOILER",
+                "CHILLER",
+                "Environment",
+                "Meter",
+            },
+        ),
+        (pytest.lazy_fixture("modified_view"), set(),),
+        (pytest.lazy_fixture("modified_simpleview"), set(),),
+        (pytest.lazy_fixture("initial_treeview"), set(),),
+        (pytest.lazy_fixture("initial_simpleview"), set(),),
+    ],
+)
+def test_expanded(qtbot, view, expanded):
+    assert expanded == view.source_model.expanded
+
+
+@pytest.mark.parametrize(
+    "view, tree_node",
+    [
+        (pytest.lazy_fixture("modified_treeview"), "key",),
+        (pytest.lazy_fixture("modified_view"), None,),
+        (pytest.lazy_fixture("modified_simpleview"), None,),
+        (pytest.lazy_fixture("initial_treeview"), "type",),
+        (pytest.lazy_fixture("initial_simpleview"), None,),
+    ],
+)
+def test_tree_node(qtbot, view, tree_node):
+    assert tree_node == view.source_model.tree_node
